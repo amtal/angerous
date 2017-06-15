dffad4bf81919d178fcaaaa778795d03e0ad65b3
amtal <alex.kropivny@gmail.com>
2017-06-15 01:25:39 -0700

    Naming

--- __init__.py
+++ __init__.py
@@ -2,9 +2,9 @@ from binaryninja.plugin import PluginCommand
 from . import liveness
 from . import interesting
 
-PluginCommand.register_for_function("ANG Liveness", 
+PluginCommand.register_for_function("Angerous Liveness", 
     "Live variables at current instruction values", liveness.liveness)
 
 # should probably register_for_range, but it looks pretty identical in UI atm
-PluginCommand.register_for_function("ANG Overview", 
+PluginCommand.register_for_function("Angerous Functions", 
     "Function table with some sorting", interesting.functions)
